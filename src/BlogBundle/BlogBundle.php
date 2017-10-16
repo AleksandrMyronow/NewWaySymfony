8d46a211a7d2eecf34f3ac53eac44be963eb9493
aleksandrmyronov <a.myronow@gmail.com>
2017-10-17 01:37:04 +0300

    first commit

---
+++ src/BlogBundle/BlogBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksandrmyronov
+ * Date: 17.10.17
+ * Time: 01:29
+ */
+
+namespace BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BlogBundle extends Bundle
+{
+
+}
